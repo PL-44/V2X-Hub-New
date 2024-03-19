4a79acf52efd95809fd91e0473ef5bd477ebb0e5
Ishan Joshi <65685284+ishanjoshi23@users.noreply.github.com>
2024-03-19 15:42:19 -0700

    Dynamic database insertions and grafana visualization (#7)

--- configuration/amd64/dataset/init.sql
+++ configuration/amd64/dataset/init.sql
@@ -1,70 +1,39 @@
-CREATE TABLE t_oil
+CREATE TABLE RoadSegments
 (
-region      text,
-country     text,
-year        int,
-production  int,
-consumption int
+    id int PRIMARY KEY,
+    speedLimitKMH int NOT NULL
 );
 
-INSERT INTO t_oil (
-region, country, year, production, consumption )
+CREATE TABLE RoadTime
+(
+    timePeriod timestamp PRIMARY KEY
+);
+
+CREATE TABLE During
+(
+    roadSegmentId int,
+    timePeriod timestamp,
+    numCars int NOT NULL,
+    postedSpeed bigint NOT NULL,
+    avgSpeed bigint NOT NULL,
+    throughput bigint NOT NULL,
+    PRIMARY KEY(roadSegmentId, timePeriod),
+    FOREIGN KEY(roadSegmentId) references RoadSegments(id),
+    FOREIGN KEY(timePeriod) references RoadTime(timePeriod)
+);
+
+INSERT INTO RoadSegments(id, speedLimitKMH) 
 VALUES
--- North America - USA
-('North America', 'USA', 1965, 9014, 11522),
-('North America', 'USA', 1966, 9579, 12100),
--- ... (continue with the rest of the values)
+(1, 50);
 
--- North America - Canada
-('North America', 'Canada', 1965, 920, 1108),
-('North America', 'Canada', 1966, 1012, 1167),
--- ... (continue with the rest of the values)
+INSERT INTO RoadTime(timePeriod) 
+VALUES
+('2024-03-05 14:34:00'),
+('2024-03-05 14:34:05'),
+('2024-03-05 14:34:10');
 
--- North America - Mexico
-('North America', 'Mexico', 1965, 362, 296),
-('North America', 'Mexico', 1966, 370, 309),
--- ... (continue with the rest of the values)
-
--- Middle East - Iran
-('Middle East', 'Iran', 1965, 1908, 134),
-('Middle East', 'Iran', 1966, 2132, 148),
--- ... (continue with the rest of the values)
-
--- Middle East - Iraq
-('Middle East', 'Iraq', 1965, 1313, NULL),
-('Middle East', 'Iraq', 1966, 1392, NULL),
--- ... (continue with the rest of the values)
-
--- Middle East - Israel
-('Middle East', 'Israel', 1965, NULL, 81),
-('Middle East', 'Israel', 1966, NULL, 84),
--- ... (continue with the rest of the values)
-
--- Middle East - Kuwait
-('Middle East', 'Kuwait', 1965, 2371, 104),
-('Middle East', 'Kuwait', 1966, 2505, 101),
--- ... (continue with the rest of the values)
-
--- Middle East - Oman
-('Middle East', 'Oman', 1965, NULL, NULL),
-('Middle East', 'Oman', 1966, NULL, NULL),
--- ... (continue with the rest of the values)
-
--- Middle East - Qatar
-('Middle East', 'Qatar', 1965, 233, 1),
-('Middle East', 'Qatar', 1966, 291, 1),
--- ... (continue with the rest of the values)
-
--- Middle East - Saudi Arabien
-('Middle East', 'Saudi Arabien', 1965, 2219, 391),
-('Middle East', 'Saudi Arabien', 1966, 2615, 394),
--- ... (continue with the rest of the values)
-
--- Middle East - Syria
-('Middle East', 'Syria', 1965, NULL, NULL),
-('Middle East', 'Syria', 1966, NULL, NULL),
--- ... (continue with the rest of the values)
-
--- Middle East - United Arab Emirates
-('Middle East', 'United Arab Emirates', 1965, 282, NULL),
-('Middle East', 'United Arab Emirates', 1966, 360, NULL);
+INSERT INTO During(roadSegmentId, timePeriod, numCars, postedSpeed, avgSpeed, throughput)
+VALUES
+(1, '2024-03-05 14:34:00', 5, 30, 27, 5),
+(1, '2024-03-05 14:34:05', 6, 30, 28, 7),
+(1, '2024-03-05 14:34:10', 6, 30, 29, 6);
--- src/v2i-hub/DatabasePlugin/src/DatabasePlugin.cpp
+++ src/v2i-hub/DatabasePlugin/src/DatabasePlugin.cpp
@@ -42,10 +42,8 @@ protected:
 	void OnConfigChanged(const char *key, const char *value);
 	void OnStateChange(IvpPluginState state);
 
-	void HandleDecodedBsmMessage(DecodedBsmMessage &msg, routeable_message &routeableMsg);
 	void HandleDataChangeMessage(DataChangeMessage &msg, routeable_message &routeableMsg);
-	void DummyInsertion();
-	// void HandleDatabaseMessage(DatabaseMessage &msg, routeable_message &routeableMsg);
+	void InsertDatabaseMessage(uint64_t timestamp, int number_of_vehicles_in_road_segment, double average_speed_of_vehicles_in_road_segment, double speed_limit_of_road_segment, double throughput_of_road_segment);
 	void OnMessageReceived(IvpMessage *msg);
 private:
 	std::atomic<uint64_t> _frequency{0};
@@ -61,9 +59,6 @@ DatabasePlugin::DatabasePlugin(string name): PluginClient(name)
 {
 	// The log level can be changed from the default here.
 	FILELog::ReportingLevel() = FILELog::FromString("DEBUG");
-
-	// Add a message filter and handler for each message this plugin wants to receive.
-	AddMessageFilter<DecodedBsmMessage>(this, &DatabasePlugin::HandleDecodedBsmMessage);
 
 	// This is an internal message type that is used to track some plugin data that changes
 	AddMessageFilter<DataChangeMessage>(this, &DatabasePlugin::HandleDataChangeMessage);
@@ -111,11 +106,6 @@ void DatabasePlugin::OnStateChange(IvpPluginState state)
 	}
 }
 
-void DatabasePlugin::HandleDecodedBsmMessage(DecodedBsmMessage &msg, routeable_message &routeableMsg)
-{
-	DummyInsertion();	
-}
-
 
 // Example of handling
 void DatabasePlugin::HandleDataChangeMessage(DataChangeMessage &msg, routeable_message &routeableMsg)
@@ -127,51 +117,6 @@ void DatabasePlugin::HandleDataChangeMessage(DataChangeMessage &msg, routeable_m
 			" to " << msg.get_untyped(msg.NewValue, to_string(_frequency));
 }
 
-void DatabasePlugin::DummyInsertion() 
-{
-	try {
-        // Connection parameters
-        const std::string host = "127.0.1.1";
-        const std::string port = "5488";
-        const std::string dbname = "my_data_wh_db";
-        const std::string user = "my_data_wh_user";
-        const std::string password = "my_data_wh_pwd";
-
-        // Construct the connection string
-        std::string conn_string = "host=" + host + " port=" + port + " dbname=" + dbname +
-                                  " user=" + user + " password=" + password;
-
-        // Establish a connection
-        pqxx::connection conn(conn_string);
-
-        if (conn.is_open()) {
-            // Create a transaction
-            pqxx::work txn(conn);
-
-            try {
-                // Perform an INSERT operation
-                txn.exec("INSERT INTO t_oil (region, country, year, production, consumption) VALUES ('TEST', 'TEST', 2021, 100, 100)");
-
-                // Commit the transaction
-                txn.commit();
-
-                std::cout << "Data inserted successfully." << std::endl;
-            } catch (const std::exception &e) {
-                // Rollback the transaction in case of an error
-                txn.abort();
-                throw;
-            }
-        } else {
-            std::cout << "Failed to open database" << std::endl;
-        }
-        
-
-        // Close the connection when done
-        conn.disconnect();
-    } catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-}
 
 void DatabasePlugin::OnMessageReceived(IvpMessage *msg) {
 
@@ -205,41 +150,63 @@ void DatabasePlugin::OnMessageReceived(IvpMessage *msg) {
 	PLOG(logDEBUG) << "Got data: " << timestamp << " " << number_of_vehicles_in_road_segment << " " << average_speed_of_vehicles_in_road_segment << " " << speed_limit_of_road_segment << " " << throughput_of_road_segment << endl;
 
 	// Push to database
-
-
+	InsertDatabaseMessage(timestamp, number_of_vehicles_in_road_segment, average_speed_of_vehicles_in_road_segment, speed_limit_of_road_segment, throughput_of_road_segment);
 }
-/* 
- * Handle Incoming Database Message 
- * Parse data here and push to database using libcq++ function.
- */
-// void DatabasePlugin::HandleDatabaseMessage(DatabaseMessage &msg, routeable_message &routeableMsg)
-// {
-// 	PLOG(logINFO) << "Received a database message: " << msg;
 
-// 	// Parse the message and push to database
-// 	// Get Fields from the message
+void DatabasePlugin::InsertDatabaseMessage(uint64_t timestamp, int number_of_vehicles_in_road_segment, double average_speed_of_vehicles_in_road_segment, double speed_limit_of_road_segment, double throughput_of_road_segment)
+{
+	try {
+        // Connection parameters
+        const std::string host = "127.0.1.1";
+        const std::string port = "5488";
+        const std::string dbname = "my_data_wh_db";
+        const std::string user = "my_data_wh_user";
+        const std::string password = "my_data_wh_pwd";
 
-// 	// Timestamp
-// 	uint64_t timestamp = msg.get_Timestamp();
+        // Construct the connection string
+        std::string conn_string = "host=" + host + " port=" + port + " dbname=" + dbname +
+                                  " user=" + user + " password=" + password;
 
-// 	// Number of vehicles in road segment
-// 	int number_of_vehicles_in_road_segment = msg.get_NumberOfVehiclesInRoadSegment();
+        // Establish a connection
+        pqxx::connection conn(conn_string);
 
-// 	// Average speed of vehicles in road segment
-// 	double average_speed_of_vehicles_in_road_segment = msg.get_AverageSpeedOfVehiclesInRoadSegment();
+        if (conn.is_open()) {
+            // Create a transaction
+            pqxx::work txn(conn);
 
-// 	// Speed limit of road segment
-// 	double speed_limit_of_road_segment = msg.get_SpeedLimitOfRoadSegment();
+            try {
+                // Perform an INSERT operation
+				const std::string initial_insert_string = "INSERT INTO During (roadSegmentId, timePeriod, numCars, postedSpeed, avgSpeed, throughput) VALUES (1, ";
+				const std::string timestamp_string = "to_timestamp(" + std::to_string(timestamp) + "/1000)";
+				const std::string number_cars_string = std::to_string(number_of_vehicles_in_road_segment);
+				const std::string posted_speed_string = std::to_string(speed_limit_of_road_segment);
+				const std::string avg_speed_string = std::to_string(average_speed_of_vehicles_in_road_segment);
+				const std::string throughput_string = std::to_string(throughput_of_road_segment);
 
-// 	// Throughput of road segment
-// 	double throughput_of_road_segment = msg.get_ThroughputOfRoadSegment();
-
-// 	// Push to database
+				txn.exec("INSERT INTO RoadTime VALUES(" + timestamp_string + ")");
+				txn.exec(initial_insert_string + timestamp_string + ", " + number_cars_string + ", " + posted_speed_string + ", " + avg_speed_string + ", " + throughput_string + ")");
 
 
+                // Commit the transaction
+                txn.commit();
 
-// }
+                PLOG(logINFO) << "Data inserted successfully.";
+            } catch (const std::exception &e) {
+                // Rollback the transaction in case of an error
+                txn.abort();
+                throw;
+            }
+        } else {
+            PLOG(logERROR) << "Failed to open database";
+        }
+        
 
+        // Close the connection when done
+        conn.disconnect();
+    } catch (const std::exception &e) {
+        PLOG(logINFO) << "Error"  << e.what();
+    }
+}
 
 // Override of main method of the plugin that should not return until the plugin exits.
 // This method does not need to be overridden if the plugin does not want to use the main thread.
@@ -248,7 +215,8 @@ int DatabasePlugin::Main()
 	PLOG(logINFO) << "Starting plugin.";
 
 	uint msCount = 0;
-	DummyInsertion();
+	InsertDatabaseMessage(1709871240197, 7, 31, 50, 6);
+
 	while (_plugin->state != IvpPluginState_error)
 	{
 		PLOG(logDEBUG4) << "Sleeping 1 ms" << endl;
